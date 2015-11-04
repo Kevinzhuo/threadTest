d849a2cd14a0ad9d41ed7cbc470b3d7066182bbe
Kevin <kevinwang@ideaculture.com>
2015-11-04 18:11:46 +0800

    commited by Kevin 2015/11/04 18:11

--- src/main/java/putTake/PutTakeTest.java
+++ src/main/java/putTake/PutTakeTest.java
@@ -24,4 +24,65 @@ public class PutTakeTest {
         this.nTrials = nTrials;
         this.nPairs = nPairs;
     }
+
+    void test() {
+        try {
+            for (int i = 0; i < nPairs; i++) {
+                pool.execute(new Producer());
+                pool.execute(new Consumer());
+            }
+            barrier.await();
+            barrier.await();
+//            assertEquals(putSum.get(), takeSum.get());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    class Producer implements Runnable {
+
+        @Override
+        public void run() {
+            try {
+                int seed = (this.hashCode() ^ (int) System.nanoTime());
+                int sum = 0;
+                for (int i = nTrials; i > 0; i++) {
+                    bb.put(seed);
+                    sum += seed;
+                    seed = xorShift(seed);
+                }
+                putSum.getAndAdd(sum);
+                barrier.await();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    class Consumer implements Runnable {
+
+        @Override
+        public void run() {
+            try {
+                barrier.await();
+                int sum = 0;
+                for (int i = nTrials; i > 0; --i) {
+                    sum += bb.take();
+                }
+                takeSum.getAndAdd(sum);
+                barrier.await();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private static int xorShift(int y) {
+        y ^= (y << 6);
+        y ^= (y >>> 21);
+        y ^= (y << 7);
+        return y;
+    }
+
 }
+
