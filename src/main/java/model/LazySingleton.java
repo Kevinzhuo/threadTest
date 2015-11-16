630568f775a40ea7521efa22b2ce29cdb2d724be
Kevinzhuo <WANGZHUO648217@gmail.com>
2015-11-16 22:41:55 +0800

    commit by Kevin 2015/11/16 22:41

---
+++ src/main/java/model/LazySingleton.java
@@ -0,0 +1,35 @@
+package model;
+
+/**
+ * Created by wangzhuo on 11/16/15.
+ */
+public class LazySingleton {
+
+    private LazySingleton() {
+        System.out.println("LazySingleton is create");
+    }
+
+    private static LazySingleton instance = null;
+
+    public static synchronized LazySingleton getInstance() {
+        if (instance == null)
+            instance = new LazySingleton();
+        return instance;
+    }
+
+    public static void main(String[] args) {
+        for (int j = 0; j < 5; j++) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    long begintime = System.currentTimeMillis();
+                    for (int i = 0; i < 100000; i++)
+                        LazySingleton.getInstance();
+                    System.out.println("spend:" + (System.currentTimeMillis() - begintime));
+                }
+            }).start();
+        }
+
+    }
+
+}
---
+++ src/main/java/model/Singleton.java
@@ -0,0 +1,26 @@
+package model;
+
+/**
+ * Created by wangzhuo on 11/16/15.
+ */
+public class Singleton {
+
+    private Singleton() {
+        System.out.println("Singleton is created");
+    }
+
+    private static Singleton instance = new Singleton();
+
+    public static Singleton getInstance() {
+        return instance;
+    }
+
+    public static void createString() {
+        System.out.println("createString in Singleton");
+    }
+
+    public static void main(String[] args) {
+        Singleton.createString();
+    }
+
+}
